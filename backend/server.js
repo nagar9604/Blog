3d5b3bb1fb023c9cfff4a43f141bc0b4ef0c2abd
shruti <nagarshruti96@gmail.com>
2025-06-26 18:06:54 +0530

    commit2

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://blog-sm.onrender.com",
     credentials:true
 }))
 
